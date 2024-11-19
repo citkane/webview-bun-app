9037ff5e94dcea7dafdbc83b2ef67ea49488a1c6
Michael Jonker <michael@openpoint.ie>
2024-11-19 20:39:07 +0000

    Update build.macos.sh

--- scripts/webview/build.macos.sh
+++ scripts/webview/build.macos.sh
@@ -16,8 +16,8 @@ TARGET_FILE="$TARGET_DIR/libwebview-macos-$(arch).dylib"
 cmake -G Ninja -B build -S . -D CMAKE_BUILD_TYPE=Release \
 -D WEBVIEW_BUILD_DOCS=false -D WEBVIEW_BUILD_EXAMPLES=false -D WEBVIEW_BUILD_TESTS=false \
 -D WEBVIEW_BUILD_STATIC_LIBRARY=false -D WEBVIEW_BUILD_AMALGAMATION=false \
--D WEBVIEW_ENABLE_CLANG_TIDY=true \
--D WEBVIEW_WEBKITGTK_API=4.1
+-D WEBVIEW_CLANG_FORMAT_EXE=.clang-format -D WEBVIEW_CLANG_TIDY_EXE=.clang-tidy
+
 
 cmake --build build
 
