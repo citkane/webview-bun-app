dbcd942455fd6d86e6862c10ebe0eb94054003fc
citkane <michael@openpoint.ie>
2024-11-19 18:15:55 +0000

    Set up scripts directory

--- scripts/webview/build.linux.sh
+++ scripts/webview/build.linux.sh
@@ -1,3 +1,27 @@
 #! /bin/bash
 
-echo hello > ../../bin/world.txt
+cd "$(dirname "$0")" || exit
+
+if [ -d "webview" ]; then
+    cd webview || exit
+else
+    git clone https://github.com/webview/webview.git
+    cd webview || exit
+fi
+
+TARGET_DIR=../../../bin/webview
+TARGET_FILE="$TARGET_DIR/libwebview-linux-$(arch).so"
+
+
+cmake -G Ninja -B build -S . -D CMAKE_BUILD_TYPE=Release \
+-D WEBVIEW_BUILD_DOCS=false -D WEBVIEW_BUILD_EXAMPLES=false -D WEBVIEW_BUILD_TESTS=false \
+-D WEBVIEW_BUILD_STATIC_LIBRARY=false -D WEBVIEW_BUILD_AMALGAMATION=false \
+-D WEBVIEW_ENABLE_CLANG_TIDY=false \
+-D WEBVIEW_WEBKITGTK_API=4.1
+
+cmake --build build
+
+cp build/core/libwebview.so "$TARGET_FILE"
+rm -rf build
+
+echo "$TARGET_FILE"
---
+++ scripts/webview/prepare.debian.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+sudo apt install libgtk-3-0 libwebkit2gtk-4.1-0
