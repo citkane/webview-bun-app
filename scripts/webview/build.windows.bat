5dd87a7fe91796ddd8339e92187f308c762d220b
Michael Jonker <michael@openpoint.ie>
2024-11-19 19:46:39 +0000

    Update build.windows.bat

--- scripts/webview/build.windows.bat
+++ scripts/webview/build.windows.bat
@@ -12,7 +12,7 @@ if exist "webview" (
 set TARGET_DIR=..\..\..\bin\webview
 set TARGET_FILE=%TARGET_DIR%\webview-windows-%PROCESSOR_ARCHITECTURE%.dll
 
-cmake -G Ninja -B build -S . -D CMAKE_BUILD_TYPE=Release ^
+cmake -G Ninja -B build -S . -Wno-dev -D CMAKE_BUILD_TYPE=Release ^
 -D WEBVIEW_BUILD_DOCS=false -D WEBVIEW_BUILD_EXAMPLES=false -D WEBVIEW_BUILD_TESTS=false ^
 -D WEBVIEW_BUILD_STATIC_LIBRARY=false -D WEBVIEW_BUILD_AMALGAMATION=false ^
 -D WEBVIEW_ENABLE_CLANG_TIDY=false
