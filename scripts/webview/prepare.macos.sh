44c4484402cec1c3bf29f370c33d3a673252c053
Michael Jonker <michael@openpoint.ie>
2024-11-19 20:44:11 +0000

    Update prepare.macos.sh

--- scripts/webview/prepare.macos.sh
+++ scripts/webview/prepare.macos.sh
@@ -1,4 +1,8 @@
 #! /bin/bash
 
 brew install ninja
-brew install clang-format
+brew install llvm
+
+ln -s "$(brew --prefix llvm)/bin/clang-format" "/usr/local/bin/clang-format"
+ln -s "$(brew --prefix llvm)/bin/clang-tidy" "/usr/local/bin/clang-tidy"
+ln -s "$(brew --prefix llvm)/bin/clang-apply-replacements" "/usr/local/bin/clang-apply-replacements"
