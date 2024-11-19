db1c5ae2d311ee270033244d56cb318dc964a4db
citkane <michael@openpoint.ie>
2024-11-19 18:44:04 +0000

    Set up scripts directory

--- scripts/webview/prepare.debian.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-sudo apt update
-sudo apt upgrade
-sudo apt install libgtk-3-0 libwebkit2gtk-4.1-0 cmake ninja-build python3 --fix-missing
---
+++ scripts/webview/prepare.ubuntu.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+sudo apt update
+sudo apt install libgtk-3-0 webkit2gtk-4.1 cmake ninja-build python3
