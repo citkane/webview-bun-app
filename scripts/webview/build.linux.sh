3ca7ffc604e5bfbd2c680adc674b2eb858bcf0bc
citkane <michael@openpoint.ie>
2024-11-19 21:12:23 +0000

    Set up scripts directory

--- scripts/webview/build.linux.sh
+++ scripts/webview/build.linux.sh
@@ -12,7 +12,6 @@ fi
 TARGET_DIR=../../../bin/webview
 TARGET_FILE="$TARGET_DIR/libwebview-linux-$(arch).so"
 
-
 cmake -G Ninja -B build -S . -D CMAKE_BUILD_TYPE=Release \
 -D WEBVIEW_BUILD_DOCS=false -D WEBVIEW_BUILD_EXAMPLES=false -D WEBVIEW_BUILD_TESTS=false \
 -D WEBVIEW_BUILD_STATIC_LIBRARY=false -D WEBVIEW_BUILD_AMALGAMATION=false \
