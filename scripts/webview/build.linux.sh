1909c361c714023f8db61dc0ce3ae769016313da
Michael Jonker <michael@openpoint.ie>
2024-11-20 10:20:50 +0000

    Update build.linux.sh

--- scripts/webview/build.linux.sh
+++ scripts/webview/build.linux.sh
@@ -12,7 +12,7 @@ fi
 TARGET_DIR=../../../bin/webview
 TARGET_FILE="$TARGET_DIR/libwebview-linux-$(arch).so"
 
-cmake -G Ninja -B build -S . -D CMAKE_BUILD_TYPE=Release \
+cmake -G Ninja -B build -S . -Wno-dev -D CMAKE_BUILD_TYPE=Release \
 -D WEBVIEW_BUILD_DOCS=false -D WEBVIEW_BUILD_EXAMPLES=false -D WEBVIEW_BUILD_TESTS=false \
 -D WEBVIEW_BUILD_STATIC_LIBRARY=false -D WEBVIEW_BUILD_AMALGAMATION=false \
 -D WEBVIEW_ENABLE_CLANG_TIDY=false -D WEBVIEW_ENABLE_CLANG_FORMAT=false \
