19ce416961029505dcae40ae50b8487916f92dee
citkane <michael@openpoint.ie>
2024-11-21 20:02:29 +0000

    Move mimeTYpes to Json and statuscodes to types

--- src/conf.ts
+++ src/conf.ts
@@ -1,16 +1,11 @@
+import mimeTypes from "./services/lib/lib.mimeTypes.json";
+
 export default {
       //responseTimeout: 500,
       windowRootTopic: "wba/window",
       logLevel: 0,
       webviewDebug: true,
-      contentTypes: {
-            html: "text/html",
-            css: "text/css",
-            js: "text/javascript",
-            mjs: "text/javascript",
-            ts: "text/javascript",
-            svg: "image/svg+xml",
-      },
+      mimeTypes,
       compileFileTypes: ["ts", "mts"],
       serverPort: 0,
       hostname: "localhost",
