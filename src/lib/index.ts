a3a17313df2b4644e8d12b1cd05661c47056b968
citkane <michael@openpoint.ie>
2024-11-21 20:03:28 +0000

    Nest lib and util under service

--- src/lib/index.ts
+++
@@ -1,5 +0,0 @@
-export { LibIpc } from "./lib.Ipc";
-export * as libServer from "./lib.server";
-export * as libPublisher from "./lib.publisher";
-export * as libSocketClient from "./lib.socketClient";
-export * as libInterface from "./lib.interface";
--- src/utils/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./util";
-export * from "./util.bunCompiler";
-export * from "./util.statusCodes";
-export { default as logger } from "./util.logger";
