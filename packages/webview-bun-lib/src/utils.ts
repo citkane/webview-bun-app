91b666b51c4e7f8cc5a44f13e1acd444abada4f1
citkane <michael@openpoint.ie>
2024-11-30 19:33:44 +0000

    Add dispatch method

--- packages/webview-bun-lib/src/utils.ts
+++ packages/webview-bun-lib/src/utils.ts
@@ -28,3 +28,42 @@ export const libFileName = join(currentDir, "../bin", _libFileName);
 export function toCstring(value: string) {
       return encoder.encode(`${value}\0`);
 }
+export function toTypedArray(object: object) {
+      const buffer = Buffer.from(JSON.stringify(object));
+      const typedArray = new Uint8Array(buffer);
+      return typedArray;
+}
+
+// @todo parsing and passing binary numbers is complicated...
+// ATM, I am just passing them as strings fot the user to convert to what their logic expects.
+/*
+export function toPointer(number: number | bigint) {
+      if (isBigInt(number)) {
+            return toCstring(number.toString());
+      }
+      if (isInteger(number)) {
+            return toCstring(number.toString());
+      }
+      if (isFloat32(number)) {
+            return toCstring(number.toString());
+      }
+      if (isFloat64(number)) {
+            return toCstring(number.toString());
+      }
+      return null;
+}
+function isBigInt(value: number | bigint) {
+      return typeof value === "bigint";
+}
+function isInteger(value: number) {
+      return Number.isInteger(value);
+}
+function isFloat32(value: number) {
+      const float32 = new Float32Array(1);
+      float32[0] = value;
+      return float32[0] === value && !Number.isInteger(value);
+}
+function isFloat64(value: number) {
+      return typeof value === "number" && !Number.isInteger(value);
+}
+*/
